fb3463a024a17956f4459a6dea872f1fe80127ad
anniethewebcoder <annie.the.web.coder@gmail.com>
2023-06-19 15:08:19 -0500

    todo list item

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const AddTodoForm = () => {
+    
+    const handleAddTodo = (event) => {
+        event.preventDefault();
+        let todoTitle = event.target.value;
+        console.log(todoTitle);
+    }
+    
     return (
         <form>
             <label htmlFor='todoTitle'>Title</label>
-            <input type='text' id='todoTitle'></input>
-            <button type='submit'>Add</button>
+            <input type='text' id='todoTitle' name='title' onSubmit={handleAddTodo}></input>
+            <button type='submit' >Add</button>
+
         </form>
     );
 }
