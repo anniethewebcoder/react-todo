a4b39dcce1d8910a12f43acc7ccbbf2089f0b83a
anniethewebcoder <annie.the.web.coder@gmail.com>
2023-08-05 22:26:45 -0400

    setting async with timeout

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { useState, useEffect } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
+const getSavedTodoList = JSON.parse(localStorage.getItem("savedTodoList"));
+
+const getAsyncTodoList = () =>
+new Promise((resolve, reject) => 
+  setTimeout(
+    () => resolve({
+      data: { todoList: getSavedTodoList }
+    }), 2000
+  )
+)
+
 function App() {
 
-  const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem("savedTodoList")) || []
-  );
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    getAsyncTodoList().then((result) => {
+      setTodoList(result.data.todoList);
+    });
+  }, []);
 
   useEffect(() => {
     localStorage.setItem("savedTodoList", JSON.stringify(todoList));
